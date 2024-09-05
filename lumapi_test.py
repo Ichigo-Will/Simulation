9607f0bc2a7fbc4c647a34ab2226167e84f050e2
Ichigo-Will <xinx1@ece.ubc.ca>
2024-09-05 16:18:28 -0700

    Update lumapi folder

--- lumapi_test.py
+++ lumapi_test.py
@@ -1,3 +1,8 @@
+import sys, os
+#default path for current release 
+sys.path.append("/Machintosh HD/Applications/Lumerical v211/Contents/API/Python") 
+sys.path.append(os.path.dirname(__file__)) #Current directory
+
 import lumapi
 
 session = lumapi.FDTD()
